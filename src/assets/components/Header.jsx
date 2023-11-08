61db579aeee83c15ae2341f85f548e7af693772a
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-07 18:17:45 -0600

    Style and position header elements

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,5 +1,4 @@
 import Logo from "../images/logo.svg";
-import DownArrow from "../images/icon-arrow-down.svg";
 import Moon from "../images/icon-moon.svg";
 
 export default function Header() {
@@ -7,10 +6,18 @@ export default function Header() {
     <header>
       <img src={Logo} alt="book logo icon" />
       <div className="header-control">
-        <p>Sans Serif</p>
-        <img src={DownArrow} alt="down arrow icon" />
-        <input type="checkbox" />
-        <img src={Moon} alt="moon icon" />
+        <div className="font-change-container">
+          <select name="font-change" className="font-change">
+            <option value="Sans Serif">Sans Serif</option>
+            <option value="Serif">Serif</option>
+            <option value="Mono">Mono</option>
+          </select>
+        </div>
+        <div className="toggle">
+          <input type="checkbox" />
+          <span className="slider"></span>
+          <img className="moon-icon" src={Moon} alt="moon icon" />
+        </div>
       </div>
     </header>
   );
