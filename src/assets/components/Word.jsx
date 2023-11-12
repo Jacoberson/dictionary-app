c5e1e17671a6575170b0257f4faa429cf99fb02f
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-12 15:43:52 -0600

    Style and position word section

--- src/assets/components/Word.jsx
+++ src/assets/components/Word.jsx
@@ -2,10 +2,12 @@ import PlayIcon from "../images/icon-play.svg";
 
 export default function Word() {
   return (
-    <section>
-      <h2>keyboard</h2>
-      <img src={PlayIcon} alt="play button" />
-      <p>pronunciation</p>
+    <section className="word-section">
+      <div className="word-info">
+        <h2 className="word">keyboard</h2>
+        <p className="pronunciation">/ˈkiːbɔːd/</p>
+      </div>
+      <img className="play-button" src={PlayIcon} alt="play button" />
     </section>
   );
 }
