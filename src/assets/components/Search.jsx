ef4435ebacc9d0f9b5a8680ac5876e6e422147a1
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-15 17:21:08 -0600

    Add state to search field

--- src/assets/components/Search.jsx
+++ src/assets/components/Search.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function Search() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <section className="search-section">
-      <input className="search" type="search" />
+      <input
+        className="search"
+        type="search"
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+      />
     </section>
   );
 }
