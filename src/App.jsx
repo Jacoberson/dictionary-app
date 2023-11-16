67e789f52c231e2131c567a6e58d8bbbbabdd398
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-16 17:51:07 -0600

    Move fetch logic to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./assets/components/Header";
 import Search from "./assets/components/Search";
 import Word from "./assets/components/Word";
 import Definition from "./assets/components/Definition";
+import { getWordInformation } from "./services/dictionary";
 import "./App.css";
 
 function App() {
   const [searchedWord, setSearchedWord] = useState("");
+  const [dictionary, setDictionary] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getWordInformation(searchedWord).then(wordInformation => {
+      if (mounted) {
+        setDictionary(wordInformation[0]);
+      }
+    });
+    return () => (mounted = false);
+  }, [searchedWord]);
 
   return (
     <div id="App" className="font-inter">
       <Header />
       <Search setSearchedWord={setSearchedWord} />
-      <Word searchedWord={searchedWord} />
+      <Word dictionary={dictionary} />
       <Definition />
     </div>
   );
--- src/assets/components/Word.jsx
+++ src/assets/components/Word.jsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
 import { PropTypes } from "prop-types";
 import PlayIcon from "../images/icon-play.svg";
-import { getWordInformation } from "../../services/dictionary";
 
 Word.propTypes = {
-  searchedWord: PropTypes.string,
+  dictionary: PropTypes.object,
 };
 
-export default function Word({ searchedWord }) {
-  const [dictionary, setDictionary] = useState([]);
-
-  useEffect(() => {
-    let mounted = true;
-    getWordInformation(searchedWord).then(wordInformation => {
-      if (mounted) {
-        setDictionary(wordInformation[0]);
-      }
-    });
-    return () => (mounted = false);
-  }, [searchedWord]);
-
+export default function Word({ dictionary }) {
   return (
     <section className="word-section">
       {dictionary && dictionary.length !== 0 ? (
