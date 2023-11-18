81f1d23aa46bf9ef76d0e9e76c24d297ed34557a
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-18 16:43:36 -0600

    Add case for when no definitions found

--- src/assets/components/Word.jsx
+++ src/assets/components/Word.jsx
@@ -45,7 +45,15 @@ export default function Word({ dictionary }) {
           />
         </>
       ) : (
-        "No word found"
+        <div className="no-definitions-found">
+          <p className="emoji">&#128577;</p>
+          <h3>No Definitions Found</h3>
+          <p>
+            Sorry pal, we couldn&apos;t find definitions for the word you were
+            looking for. You can try the search again at a later time or head to
+            the web instead.
+          </p>
+        </div>
       )}
     </section>
   );
